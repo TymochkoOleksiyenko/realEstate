bd3e57b54e289b750a2b623da80ec46cf9c9ffcb
vova99 <mr.oleksiyenko@gmail.com>
2021-05-25 22:03:55 +0300

    init

--- src/main/java/com/realEstate/jpa/FlatJPA.java
+++ src/main/java/com/realEstate/jpa/FlatJPA.java
@@ -3,5 +3,8 @@ package com.realEstate.jpa;
 import com.realEstate.entity.Flat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FlatJPA extends JpaRepository<Flat,Integer> {
+    List<Flat> findByCreatedById(int id);
 }
